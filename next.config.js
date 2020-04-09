04b2ffbf309fec8520b1412613a72a653b266d80
Kuzey Köse <kuzeykose@Kuzeys-MacBook-Pro.local>
2020-04-09 11:24:13 +0300

    trying netlifyv2

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 const withCSS = require('@zeit/next-css');
+const client = require('./client')
 
 module.exports = withCSS({
   webpack: config => {
@@ -7,6 +8,24 @@ module.exports = withCSS({
       use: "raw-loader"
     });
     return config;
+  },
+  exportPathMap: async function (defaultPathMap) {
+    const path = await client
+      // get all the posts and return those with slugs
+      .fetch('*[_type == "post"].slug.current')
+      .then(data =>
+        // use reduce to build an object with routes
+        // and select the blog.js file, and send in the
+        // correct query paramater.
+        data.reduce(
+          (acc, slug) => ({
+            '/': { page: '/' }
+          }),
+          {}
+        )
+      )
+      .catch(console.error)
+    return path
   }
 });
 
