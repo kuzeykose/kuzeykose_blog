7ab9f5327d1cddad50bc9fe9a8e9c8c5bffd1912
Kuzey Köse <kuzeykose@Kuzeys-MacBook-Pro.local>
2020-04-09 11:11:46 +0300

    trying netlify

--- next.config.js
+++ next.config.js
@@ -9,5 +9,23 @@ module.exports = withCSS({
     return config;
   },
 
+  exportPathMap: async function (defaultPathMap) {
+    const path = await client
+      // get all the posts and return those with slugs
+      .fetch('*[_type == "post"].slug.current')
+      .then(data =>
+        // use reduce to build an object with routes
+        // and select the blog.js file, and send in the
+        // correct query paramater.
+        data.reduce(
+          (acc, slug) => ({
+            '/': { page: '/' },
+          }),
+          {}
+        )
+      )
+      .catch(console.error)
+    return path
+  }
 });
 
