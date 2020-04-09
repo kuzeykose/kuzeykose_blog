42847089428a444a8ad9e1a5b7e06cff84e85246
Kuzey Köse <kuzeykose@Kuzeys-MacBook-Pro.local>
2020-04-09 11:26:55 +0300

    tryingv3 netlify

--- next.config.js
+++ next.config.js
@@ -8,24 +8,6 @@ module.exports = withCSS({
       use: "raw-loader"
     });
     return config;
-  },
-  exportPathMap: async function (defaultPathMap) {
-    const path = await client
-      // get all the posts and return those with slugs
-      .fetch('*[_type == "post"].slug.current')
-      .then(data =>
-        // use reduce to build an object with routes
-        // and select the blog.js file, and send in the
-        // correct query paramater.
-        data.reduce(
-          (acc, slug) => ({
-            '/': { page: '/' }
-          }),
-          {}
-        )
-      )
-      .catch(console.error)
-    return path
   }
 });
 
