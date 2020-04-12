5a9f18fdabf90a7b83590b4b0f4c05b5e403b320
Kuzey Köse <kuzeykose@Kuzeys-MacBook-Pro.local>
2020-04-12 14:20:01 +0300

    netlify dyn pages

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 const withCSS = require('@zeit/next-css');
+const jdown = require('jdown');
 
 module.exports = withCSS({
   webpack: config => {
@@ -7,6 +8,26 @@ module.exports = withCSS({
       use: "raw-loader"
     });
     return config;
+  },
+  exportPathMap: async function () {
+    // pages we know about beforehand
+    const paths = {
+      '/': { page: '/' },
+      '/hakkimmda': { page: '/hakkimda' }
+    }
+    // dynamic, data-generated pages
+    const content = await jdown('src/posts') // assumes some markdown files in a `/content` folder, with frontmatter that offers a slug
+    const posts = [] // build up array of objects for the top level list
+    Object.entries(content).forEach(([filename, fileContent]) => {
+      // the filename becomes the slug
+      paths[`/${filename}`] = {
+        page: '/[postId]', query: {
+          postId: filename,
+          ...fileContent
+        }
+      }
+    })
+    return paths
   }
 });
 
