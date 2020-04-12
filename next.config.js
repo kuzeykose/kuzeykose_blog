66c2d652dc4ba2bd0c2a7a24a6b4edebed68b398
Kuzey Köse <kuzeykose@Kuzeys-MacBook-Pro.local>
2020-04-12 14:55:32 +0300

    -> postID config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 const withCSS = require('@zeit/next-css');
-const myposts = require('./src/blog-posts');
+import blogposts from './src/blog-posts'
 
 module.exports = withCSS({
   webpack: config => {
@@ -13,15 +13,12 @@ module.exports = withCSS({
     // pages we know about beforehand
     const paths = {
       '/': { page: '/' },
-      '/hakkimda': { page: '/hakkimda' }
+      '/Hakkimda': { page: '/Hakkimda' }
     }
-    // dynamic, data-generated pages
-    const content = myposts // assumes some markdown files in a `/content` folder, with frontmatter that offers a slug
-    const posts = [] // build up array of objects for the top level list
-    Object.entries(content).forEach(([filename]) => {
-      // the filename becomes the slug
-      paths[`/${filename}`] = {
-        page: '/api/post/${postId}', query: { postId: filename }
+    const blogs = blogposts
+    Object.entries(blogs).forEach(([postId]) => {
+      paths[`/${postId}`] = {
+        page: '/api/post/[postId]', query: { postId: postId }
       }
     })
     return paths
