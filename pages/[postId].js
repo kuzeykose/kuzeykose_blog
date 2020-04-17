97bd08d4b9ad71fcd7745fd2a64cf83d96858e0e
Kuzey Köse <kuzeykose@Kuzeys-MacBook-Pro.local>
2020-04-17 23:03:46 +0300

    aba

--- pages/[postId].js
+++ pages/[postId].js
@@ -33,7 +33,7 @@ const BlogPost = ({ post }) => (
 
 BlogPost.getInitialProps = async ({ req, query }) => {
   //kuzeykose.com
-  const res = await fetch(`https://app.netlify.com/sites/confident-euclid-be6319/overview/api/post/${query.postId}`);
+  const res = await fetch(`https://kuzeykose-blog.now.sh/api/post/${query.postId}`);
   const json = await res.json();
   return { post: json.post };
 };
